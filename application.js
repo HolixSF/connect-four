0f524b6b75f80bb32a47f23a9fc36e392b0ed19f
Albert Hahn & Sean Massih <pair+albhahn.seanfromiran@devbootcamp.com>
2016-01-28 13:46:58 -0800

    add first win check

--- application.js
+++ application.js
@@ -1,29 +1,26 @@
 var turn = 0;
+var redChecker = '<svg height="100" width="100"> <circle cx="50" cy="50" r="40" fill="#ff4040" /> </svg>';
+var blackChecker = '<svg height="100" width="100"> <circle cx="50" cy="50" r="40" fill="#404040" /> </svg>';
 
 $(document).ready(function () {
   $('button').on('click', function(e){
     e.preventDefault;
 
-    var table = $('table')[0];
-    var row = $(this).parent().children().index($(this));
-    var col = $(this).parent().index();
-    // alert('Row: ' + row + ', Column: ' + col);
-    // console.log($(this).parent().index());
+    var $table = $('table')[0];
+    var $row = $(this).index();
+    var $col = $(this).parent().index();
+    var $checkTopCell = $($table.rows[1].cells[$col]);
 
-    var checkTopCell = table.rows[1].cells[col];
-    var $checkTopCell = $(checkTopCell);
     if ($checkTopCell.html()) {
-      // console.log($checkTopCell.html());
       alert('This column is full');
     } else {
-      for(i = 6; i >= 1; i--){
-        var checkCell = table.rows[i].cells[col];
-        var $checkCell = $(checkCell);
+      for (i = 6; i >= 1; i--) {
+        var $checkCell = $($table.rows[i].cells[$col]);
         if (!$checkCell.html()) {
-          if (turn%2==0){
-            $checkCell.html('<svg height="100" width="100"> <circle cx="50" cy="50" r="40" fill="#ff4040" /> </svg>');
+          if (turn%2==0) {
+            $checkCell.html(redChecker);
           } else {
-            $checkCell.html('<svg height="100" width="100"> <circle cx="50" cy="50" r="40" fill="#404040" /> </svg>');
+            $checkCell.html(blackChecker);
           }
           turn++;
           break ;
@@ -31,6 +28,16 @@ $(document).ready(function () {
       }
     };
 
+    if ($checkCell) {
+      if ($col >= 3) {
+        var $cellOneToLeft = $($table.rows[$checkCell.parent().index()].cells[$col-1]);
+        var $cellTwoToLeft = $($table.rows[$checkCell.parent().index()].cells[$col-2]);
+        var $cellThreeToLeft = $($table.rows[$checkCell.parent().index()].cells[$col-3]);
+        if (($checkCell.html()==$cellOneToLeft.html())&&($cellOneToLeft.html()==$cellTwoToLeft.html())&&($cellTwoToLeft.html()==$cellThreeToLeft.html())){
+          alert('You won!');
+        };
+      };
+    };
   });
 
 });

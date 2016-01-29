32bec990953383e7010a44dbd1adeee049bb9eac
Luis De Castro & Shawn Spears <pair+holixsf.shawnspears@devbootcamp.com>
2016-01-29 12:01:32 -0800

    Add music and play button

--- application.js
+++ application.js
@@ -1,3 +1,4 @@
+var audio = new Audio('test-your-skills-short.wav')
 var turn = 0;
 var gameWon = true;
 var redChecker = '<svg height="100" width="100"> <circle cx="50" cy="50" r="40" fill="#ff4040" /> </svg>';
@@ -11,33 +12,49 @@ Array.prototype.allValuesSame = function() {
 }
 
 $(document).ready(function () {
-  $('button').on('click', function(e){
-    if (gameWon) {
-      e.preventDefault;
+  bindListeners()
+});
 
-      var $table = $('table')[0];
-      var $row = $(this).index();
-      var $col = $(this).parent().index();
-      var $checkTopCell = $($table.rows[1].cells[$col]);
+var bindListeners = function(){
+  $('.glyphicon-play').on('click', displayBoardGame)
+  $('button').on('click', playGame)
+}
 
-      if ($checkTopCell.html()) {
-        alert('This column is full');
-      } else {
-        for (i = 6; i >= 1; i--) {
-          var $checkCell = $($table.rows[i].cells[$col]);
-          if (!$checkCell.html()) {
-            if (turn%2==0) {
-              $checkCell.html(redChecker);
-            } else {
-              $checkCell.html(blackChecker);
-            }
-            turn++;
-            break ;
+var displayBoardGame = function(e){
+  e.preventDefault;
+  $('.button-container').remove();
+  $('#connect-four-table').show();
+}
+
+var playGame = function(e){
+  if (gameWon) {
+    e.preventDefault;
+    audio.loop = true;
+    audio.play();
+
+    var $table = $('table')[0];
+    var $row = $(this).index();
+    var $col = $(this).parent().index();
+    var $checkTopCell = $($table.rows[1].cells[$col]);
+
+    if ($checkTopCell.html()) {
+      alert('This column is full');
+    } else {
+      for (i = 6; i >= 1; i--) {
+        var $checkCell = $($table.rows[i].cells[$col]);
+        if (!$checkCell.html()) {
+          if (turn%2==0) {
+            $checkCell.html(redChecker);
+          } else {
+            $checkCell.html(blackChecker);
           }
+          turn++;
+          break ;
         }
-      };
+      }
+    };
 
-      if ($checkCell) {
+    if ($checkCell) {
         // Variables for checking win conditions
         var $cellOneToLeft = $($table.rows[$checkCell.parent().index()].cells[$col-1]);
         var $cellTwoToLeft = $($table.rows[$checkCell.parent().index()].cells[$col-2]);
@@ -96,81 +113,93 @@ $(document).ready(function () {
           if ([$checkCell.html(),$cellOneToLeft.html(),$cellTwoToLeft.html(),$cellThreeToLeft.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($col >= 2 && $col <= 5) {
           if ([$checkCell.html(),$cellOneToLeft.html(),$cellTwoToLeft.html(),$cellOneToRight.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($col >= 1 && $col <= 4) {
           if ([$checkCell.html(),$cellOneToLeft.html(),$cellTwoToRight.html(),$cellOneToRight.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($col <= 3) {
           if ([$checkCell.html(),$cellOneToRight.html(),$cellTwoToRight.html(),$cellThreeToRight.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($checkCell.parent().index() <= 3) {
           if ([$checkCell.html(),$cellOneDown.html(),$cellTwoDown.html(),$cellThreeDown.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($checkCell.parent().index() >= 4) {
           if ([$checkCell.html(),$cellOneUpOneRight.html(),$cellTwoUpTwoRight.html(),$cellThreeUpThreeRight.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($checkCell.parent().index() <= 3) {
           if ([$checkCell.html(),$cellOneDownOneRight.html(),$cellTwoDownTwoRight.html(),$cellThreeDownThreeRight.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($checkCell.parent().index() >= 4) {
           if ([$checkCell.html(),$cellOneUpOneLeft.html(),$cellTwoUpTwoLeft.html(),$cellThreeUpThreeLeft.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($checkCell.parent().index() <= 3) {
           if ([$checkCell.html(),$cellOneDownOneLeft.html(),$cellTwoDownTwoLeft.html(),$cellThreeDownThreeLeft.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($checkCell.parent().index() >= 3 && $checkCell.parent().index() <= 5) {
           if ([$checkCell.html(),$cellOneDownOneLeft.html(),$cellOneUpOneRight.html(),$cellTwoUpTwoRight.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($checkCell.parent().index() >= 2 && $checkCell.parent().index() <= 4) {
           if ([$checkCell.html(),$cellOneDownOneLeft.html(),$cellOneUpOneRight.html(),$cellTwoDownTwoLeft.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($checkCell.parent().index() >= 2 && $checkCell.parent().index() <= 4) {
           if ([$checkCell.html(),$cellOneUpOneLeft.html(),$cellOneDownOneRight.html(),$cellTwoDownTwoRight.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
         if ($checkCell.parent().index() >= 3 && $checkCell.parent().index() <= 5) {
           if ([$checkCell.html(),$cellOneUpOneLeft.html(),$cellOneDownOneRight.html(),$cellTwoUpTwoLeft.html()].allValuesSame()){
             alert('You won!');
             gameWon = false;
+            audio.pause();
           };
         };
       };
     };
-  });
-});
+  }
